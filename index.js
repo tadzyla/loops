50de0c000b9dae5e78e4b4f2ca521145b67d6a29
tadzyla <tadas.zvinklys@gmail.com>
2022-11-28 17:28:37 +0000

    adding functions

--- index.js
+++ index.js
@@ -3,14 +3,29 @@ const largerNumber = (firstNumber, secondNumber) => {
 
     let biggerNumber = 0;
 
-    if( firstNumber > secondNumber ) {
+    if ( firstNumber > secondNumber ) {
         biggerNumber = firstNumber;
     } else {
-        biggerNumber = secondNumber
+        biggerNumber = secondNumber;
     }
 
     console.log(biggerNumber);
     return biggerNumber;
 }
 
+const isEvenNumber = (number) => {
+
+    let resultNumber = '';
+
+    if ( number % 2 === 0 ) {
+        resultNumber = 'Even number'
+    } else {
+        resultNumber = 'Odd number'
+    }
+
+    console.log(resultNumber);
+    return resultNumber;
+}
+
 largerNumber(8, 2);
+isEvenNumber(44);
